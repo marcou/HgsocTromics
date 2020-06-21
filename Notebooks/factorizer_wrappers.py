b3026dcc8df9f0a31fb15e615ffe8dfd135354fa
Ian Poole <ipoole@icloud.com>
2020-06-21 15:00:39 +0100

    Various... * aocs_expression.ipynb: selection of top ranked genes for enrichment analysis changed from 100 to 3 * SD. * factor_angle_analysis.ipynb:   * multiple runs for ICA and NMF run and saved as pickle files   * t-SNE plots of combined ICA and NMF clusters   * demonstration that k-means can identify clusters in PCA reduced dimensions   * printing table of angles between components (in reduced PCA dimensions) * factorizer_wrappers.py: added Nimfa_NMF_Factorizer (not yet used in anger).  Also reduced NMF tol 0.0001 -> 0.01

--- Notebooks/factorizer_wrappers.py
+++ Notebooks/factorizer_wrappers.py
@@ -1,13 +1,14 @@
 from os import path
 import numpy as np
 from sklearn.decomposition import NMF, FastICA, PCA
+import nimfa
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Make wrapper classes for FastICA and NMF, so we can interface to them identically
 class ICA_Factorizer(FastICA):
     def __init__(self, n_components=None, max_iter=200, 
-                 random_state=42, fun='logcosh'):
+                 random_state=42, fun='logcosh', algorithm='parallel'):
         FastICA.__init__(self, n_components=n_components, max_iter=max_iter,
                         random_state=random_state, fun=fun)
         self.V = None
@@ -43,9 +44,9 @@ class ICA_Factorizer(FastICA):
         return self.recovered_V
 
 class NMF_Factorizer(NMF):
-    def __init__(self, n_components=None, max_iter=200, random_state=42):
+    def __init__(self, n_components=None, max_iter=200, random_state=42, tol=0.01):
         NMF.__init__(self, n_components=n_components, max_iter=max_iter, 
-                     random_state=random_state)
+                     random_state=random_state, tol=tol)
         self.V = None
         self.W = None
         self.H = None
@@ -75,6 +76,43 @@ class NMF_Factorizer(NMF):
             H = self.get_H()
             self.recovered_V = np.dot(W, H)
             #print(self.recovered_V)
+        return self.recovered_V
+
+class Nimfa_NMF_Factorizer(object):
+    """This version uses the nimfa package """
+    def __init__(self, n_components=None, max_iter=1000, random_state=None):
+    
+        self.n_components = n_components
+        self.max_iter = max_iter
+        self.random_state = random_state
+        
+        self.V = None
+        self.W = None
+        self.H = None
+        self.recovered_V = None
+        
+    def fit(self, V):
+        self.V = V
+        self.lsnmf = nimfa.Bmf(self.V, seed='random_vcol', max_iter=self.max_iter, rank=self.n_components)
+        self.lsnmf_fit = self.lsnmf()
+        self.W = self.lsnmf_fit.basis()
+        self.H = self.lsnmf_fit.coef()
+        return self
+        
+    def get_W(self):
+        assert self.W is not None
+        return self.W
+    
+    def get_H(self):
+        assert self.H is not None
+        return self.H
+    
+    def get_recovered_V(self):
+        assert self.V is not None
+        if self.recovered_V is None:
+            W = self.get_W()
+            H = self.get_H()
+            self.recovered_V = np.dot(W, H)
         return self.recovered_V
 
 class PCA_Factorizer(PCA):
