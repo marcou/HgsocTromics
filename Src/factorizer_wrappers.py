2b3d4ea698c548a5955b5d97f3f93f2b218bf504
Ian Poole <ipoole@icloud.com>
2020-06-27 16:47:46 +0100

    Bootstrap resampling and other cleanup * factor_clustering.py:   * Use resampling with replacement for each repeat calculation (bootstrapping)   * Apply to PCA also, thus three methods treated the same   * Include short basename in plot titles   * Various other tidy * test_factor_clustering.py: added test for plot_multiple_combined_factors_scatter, and general improvements * factorizer_wrappers.py: tidied up constructors and fit() methods for consistency * factor_clustering_results.ipynb: modified for above changes.  Ran for HGSOC dataset and html saved (2)

--- Src/factorizer_wrappers.py
+++ Src/factorizer_wrappers.py
@@ -1,38 +1,40 @@
-from os import path
 import numpy as np
 from sklearn.decomposition import NMF, FastICA, PCA
 import nimfa
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
 # Make wrapper classes for FastICA and NMF, so we can interface to them identically
+# noinspection PyPep8Naming,PyPep8Naming
 class ICA_Factorizer(FastICA):
-    def __init__(self, n_components=None, max_iter=200, tol=0.01,
-                 random_state=42, fun='logcosh'):
+    def __init__(self, n_components=None, max_iter=200, tol=0.01, random_state=42, fun='logcosh'):
         FastICA.__init__(self, n_components=n_components, max_iter=max_iter, tol=tol,
-                        random_state=random_state, fun=fun)
+                         random_state=random_state, fun=fun)
         self.V = None
         self.W = None
         self.H = None
         self.recovered_V = None
-        
-    def fit(self, V):
+
+    def fit(self, V, nu=None):
+        assert nu is None
         self.V = V
         self.W = self.fit_transform(V)
         return self
-        
+
     def get_W(self):
         assert self.V is not None
         if self.W is None:
             self.W = self.fit_transform(self.V)
         return self.W
-    
+
     def get_H(self):
         assert self.V is not None
         if self.H is None:
             self.H = self.mixing_.T
         return self.H
-    
+
     def get_recovered_V(self):
         assert self.V is not None
         if self.recovered_V is None:
@@ -40,73 +42,83 @@ class ICA_Factorizer(FastICA):
             H = self.get_H()
             mean = self.mean_
             self.recovered_V = np.dot(W, H) + mean
-            #print(self.recovered_V)
+            # print(self.recovered_V)
         return self.recovered_V
 
+
+# noinspection PyPep8Naming
 class NMF_Factorizer(NMF):
-    def __init__(self, n_components=None, max_iter=200, random_state=42, tol=0.01):
-        NMF.__init__(self, n_components=n_components, max_iter=max_iter, 
+    def __init__(self, n_components=None, max_iter=200, tol=0.01, random_state=42):
+        NMF.__init__(self, n_components=n_components, max_iter=max_iter,
                      random_state=random_state, tol=tol)
         self.V = None
         self.W = None
         self.H = None
         self.recovered_V = None
-        
-    def fit(self, V):
+
+    def fit(self, V, nu1=None, nu2=None):
+        assert nu1 is None and nu2 is None
         self.V = V
         self.W = self.fit_transform(V)
         return self
-        
+
     def get_W(self):
         assert self.V is not None
         if self.W is None:
             self.W = self.fit_transform(self.V)
         return self.W
-    
+
     def get_H(self):
         assert self.V is not None
         if self.H is None:
             self.H = self.components_
         return self.H
-    
+
     def get_recovered_V(self):
         assert self.V is not None
         if self.recovered_V is None:
             W = self.get_W()
             H = self.get_H()
             self.recovered_V = np.dot(W, H)
-            #print(self.recovered_V)
+            # print(self.recovered_V)
         return self.recovered_V
 
+
+# noinspection PyPep8Naming
 class Nimfa_NMF_Factorizer(object):
     """This version uses the nimfa package """
-    def __init__(self, n_components=None, max_iter=1000, random_state=None):
-    
+
+    # noinspection PyUnusedLocal
+    def __init__(self, n_components=None, max_iter=200, tol=None, random_state=None):
         self.n_components = n_components
         self.max_iter = max_iter
         self.random_state = random_state
-        
+
         self.V = None
         self.W = None
         self.H = None
+        self.lsnmf = None
+        self.lsnmf_fit = None
         self.recovered_V = None
-        
-    def fit(self, V):
+
+    def fit(self, V, nu=None):
+        assert nu is None
         self.V = V
-        self.lsnmf = nimfa.Bmf(self.V, seed='random_vcol', max_iter=self.max_iter, rank=self.n_components)
+        self.lsnmf = nimfa.Bmf(self.V, seed='random_vcol', max_iter=self.max_iter,
+                               rank=self.n_components)
         self.lsnmf_fit = self.lsnmf()
         self.W = self.lsnmf_fit.basis()
         self.H = self.lsnmf_fit.coef()
         return self
-        
+
     def get_W(self):
         assert self.W is not None
         return self.W
-    
+
     def get_H(self):
         assert self.H is not None
         return self.H
-    
+
     def get_recovered_V(self):
         assert self.V is not None
         if self.recovered_V is None:
@@ -115,39 +127,39 @@ class Nimfa_NMF_Factorizer(object):
             self.recovered_V = np.dot(W, H)
         return self.recovered_V
 
+
+# noinspection PyPep8Naming
 class PCA_Factorizer(PCA):
-    def __init__(self, n_components=None, max_iter=None, random_state=None):
+    # noinspection PyUnusedLocal,PyUnusedLocal,PyUnusedLocal
+    def __init__(self, n_components=None, max_iter=None, tol=None, random_state=None):
         PCA.__init__(self, n_components=n_components)
         self.V = None
         self.W = None
         self.H = None
         self.recovered_V = None
-        
-    def fit(self, V):
+
+    def fit(self, V, nu=None):
+        assert nu is None
         self.V = V
         self.W = self.fit_transform(V)
         return self
-        
+
     def get_W(self):
         assert self.V is not None
         if self.W is None:
             self.W = self.fit_transform(self.V)
         return self.W
-    
+
     def get_H(self):
         assert self.V is not None
         if self.H is None:
             self.H = self.components_
         return self.H
-    
+
     def get_recovered_V(self):
         assert self.V is not None
         if self.recovered_V is None:
             W = self.get_W()
             H = self.get_H()
-            self.recovered_V = np.dot(W,H) + self.mean_
+            self.recovered_V = np.dot(W, H) + self.mean_
         return self.recovered_V
-
-
-
-
--- Src/tests/test_factor_clustering.py
+++ Src/tests/test_factor_clustering.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 import numpy as np
 
 from factor_clustering import FactorClustering
-from factorizer_wrappers import ICA_Factorizer, NMF_Factorizer
+from factorizer_wrappers import ICA_Factorizer, NMF_Factorizer, PCA_Factorizer
 
 
 class TestFactorClustering(TestCase):
@@ -49,23 +49,28 @@ class TestFactorClustering(TestCase):
 
     def test_compute_and_cache_one_factor_repeats(self):
         fc = self.clustering()
-        pickle_fname = fc.compute_and_cache_one_factor_repeats(
-            fc.expression_matrix, ICA_Factorizer, 4, 2)
-        assert os.path.exists(pickle_fname)
-        metagene_list = fc.read_cached_factors(ICA_Factorizer, 4, 2)
-        # Ensure there is randomness in the repeat results!
-        assert not np.array_equal(metagene_list[0], metagene_list[1])
+        n_components = 4
+        n_repeats = 10
 
-        metagene_list_2 = fc.read_cached_factors(ICA_Factorizer, 4, 2)
-        assert len(metagene_list_2) == 2
+        def one_test(facto_class):
+            pkl_fname = fc.compute_and_cache_one_factor_repeats(
+                facto_class, n_components, n_repeats)
+            assert os.path.exists(pkl_fname)
+            metagene_list = fc.read_cached_factors(ICA_Factorizer, n_components, n_repeats)
+            # Ensure there is randomness in the repeat results!
+            assert not np.array_equal(metagene_list[0], metagene_list[1])
+            metagene_list_2 = fc.read_cached_factors(facto_class, n_components, n_repeats)
+            assert len(metagene_list_2) == n_repeats
+
+        one_test(NMF_Factorizer)
+        one_test(PCA_Factorizer)
 
     def test_single_factor_scatter(self):
         fc = self.clustering()
         n_repeats = 10
         n_components = 3
         facto = ICA_Factorizer
-        fc.compute_and_cache_one_factor_repeats(
-            fc.expression_matrix, facto, n_components, n_repeats, force=False)
+        fc.compute_and_cache_one_factor_repeats(facto, n_components, n_repeats, force=False)
 
         fc.single_factor_scatter(facto, n_components, n_repeats, show=False)
 
@@ -73,13 +78,21 @@ class TestFactorClustering(TestCase):
         fc = self.clustering()
         n_repeats = 10
         fc.compute_and_cache_multiple_factor_repeats(4, 5, n_repeats=n_repeats, force=False)
-        fc.combined_factors_scatter(4, n_repeats=n_repeats)
+        fc.combined_factors_scatter(4, n_repeats=n_repeats, show=True)
+
+    def test_plot_multiple_combined_factors_scatter(self):
+        n_components_range = 2, 4
+        n_repeats = 10
+        fc = self.clustering()
+        fc.compute_and_cache_multiple_factor_repeats(*n_components_range, n_repeats, force=False)
+        fc.plot_multiple_combined_factors_scatter(*n_components_range, n_repeats, show=True)
 
     def test_investigate_cluster_statistics(self):
+        n_components = 3
+        n_repeats = 10
         fc = self.clustering()
-        fc.compute_and_cache_one_factor_repeats(
-            fc.expression_matrix, NMF_Factorizer, 4, 2)
-        result = fc.investigate_cluster_statistics(NMF_Factorizer, 4, 2)
+        fc.compute_and_cache_one_factor_repeats(NMF_Factorizer, n_components, n_repeats)
+        result = fc.investigate_cluster_statistics(NMF_Factorizer, n_components, n_repeats)
         print(result)
 
     def test_compute_silhouette_score_and_median(self):
@@ -87,8 +100,7 @@ class TestFactorClustering(TestCase):
         n_components = 3
         facto_class = NMF_Factorizer
         fc = self.clustering()
-        fc.compute_and_cache_one_factor_repeats(
-            fc.expression_matrix, facto_class, n_components, n_repeats, force=False)
+        fc.compute_and_cache_one_factor_repeats(facto_class, n_components, n_repeats, force=False)
         score, median_metagenes = fc.compute_silhouette_score_and_median(
             NMF_Factorizer, n_components, n_repeats, doprint=False)
         print("Score = %8.6f" % score)
@@ -100,9 +112,9 @@ class TestFactorClustering(TestCase):
         fc.save_multiple_median_metagenes_to_factors(NMF_Factorizer, start=2, end=5, n_repeats=10)
 
     def test_find_best_n_components(self):
+        n_components_range = 2, 4
         n_repeats = 10
         facto_class = NMF_Factorizer
-        n_components_range = 2, 4
         fc = self.clustering()
         fc.compute_and_cache_multiple_factor_repeats(*n_components_range, n_repeats, force=False)
         fc.find_best_n_components(facto_class, *n_components_range, n_repeats,
