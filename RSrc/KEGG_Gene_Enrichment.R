ec546831ff4ed97403b31813b5bc4f014afb71cb
Ian Poole <ipoole@icloud.com>
2020-07-03 18:43:07 +0100

    KEGG enrichment R code now working for multiple components, outputing a combined .tsv file.  Crap code!

--- RSrc/KEGG_Gene_Enrichment.R
+++ RSrc/KEGG_Gene_Enrichment.R
@@ -43,10 +43,24 @@ getKeyGenes<-function(full,IC){
 
 }
 
+# ====== Main Program =========
+
+setwd("/home/ipoole/Documents/gitrepos/HgsocTromics/RSrc")
+
+datasetname = 'AOCS_Protein'
+facto = 'ICA'
+n_components = 3
+
+factor_file_path = sprintf('../Factors/%s/%s_median_factor_%d.tsv', datasetname, facto, n_components)
+results_file_path = sprintf('../Cache/%s/GeneEnrichment/%s_%d_kegg_ge_all.tsv', datasetname, facto, n_components)
+dir.create(dirname(factor_file_path))
+dir.create(dirname(results_file_path))
+cat('Starting ...')
+
 ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
                        
-#Load your metagene matrix with genes as rows and genenames as rownames, ICs as columns with column names "IC1","IC2"... or something similar
-ICs<-read.table("/home/ipoole/Documents/gitrepos/HgsocTromics/Factors/TCGA_OV_VST/ICA_median_factor_3.tsv",header=T,row.names=1,sep="\t")
+# Load your metagene matrix with genes as rows and genenames as rownames, ICs as columns with column names "IC1","IC2"... or something similar
+ICs<-read.table(factor_file_path, header=T, row.names=1, sep="\t")
     
 # Get the list of ensembl IDs in our study
 all_ensembl<-rownames(ICs)
@@ -58,36 +72,57 @@ gene_anno_all<-getBM(attributes=c('ensembl_gene_id', 'entrezgene_id', 'hgnc_symb
 hugo_genes_all<-as.character(gene_anno_all[gene_anno_all[,'ensembl_gene_id'] %in% all_ensembl,'entrezgene_id'])
 
 # We'll deal with just the first of three components.  getKeyGenes() returns a dataframe with ''above''Direction' column ('above' or 'below')
-keygenes<-getKeyGenes(ICs,1)
-genes<-as.character(keygenes[is.na(keygenes[,'Genes'])==FALSE,1])
+kegg_results <- list()
+for (ic in 1: n_components){
+    keygenes<-getKeyGenes(ICs, ic)
+    genes<-as.character(keygenes[is.na(keygenes[,'Genes'])==FALSE,1])
+    
+    # genes is now a simple list of entrez gene ids.  Need to annotate same as for gene_anno above
+    gene_anno<-getBM(attributes=c('ensembl_gene_id', 'entrezgene_id', 'hgnc_symbol'), filters='ensembl_gene_id', values=genes, mart=ensembl)
+    hugo_genes<-unique(as.character(gene_anno[gene_anno[,'ensembl_gene_id'] %in% genes, 'entrezgene_id']))
+    
+    # So we can now pass these genes to gene_enrichment function
+    
+    kegg_results[[ic]] <- clusterProfiler::enrichKEGG(
+                    gene          = hugo_genes,
+                    universe      = hugo_genes_all,
+                    organism         = 'hsa',
+                    pAdjustMethod = "BH",
+                    pvalueCutoff  = 0.05,
+                    qvalueCutoff  = 0.05)
+    
+    # ekegg_readable = setReadable(ekegg, 'org.Hs.eg.db', 'ENTREZID')
+    
+    cat(sprintf("KEGG Gene Enrichment Analysis Done for %s, component %d, %d resuts found.", factor_file_path, ic, length(ekegg)))
+}
 
-# genes is now a simple list of entrez gene ids.  Need to annotate same as for gene_anno above
-gene_anno<-getBM(attributes=c('ensembl_gene_id', 'entrezgene_id', 'hgnc_symbol'), filters='ensembl_gene_id', values=genes, mart=ensembl)
-hugo_genes<-unique(as.character(gene_anno[gene_anno[,'ensembl_gene_id'] %in% genes, 'entrezgene_id']))
+# Add columns for factorizer method and component then concatonate rows
+kegg_dfs <- list()
+for (ic in 1: n_components){
+    readable_kegg = setReadable(kegg_results[[ic]], 'org.Hs.eg.db', 'ENTREZID')
+    df = as.data.frame(readable_kegg)
+    df['Component'] = ic-1
+    df['Factorizer'] = facto
+    kegg_dfs[[ic]] = df
+}
 
-# So we can now pass these genes to gene_enrichment function
-ekegg <- clusterProfiler::enrichKEGG(
-                gene          = hugo_genes,
-                universe      = hugo_genes_all,
-                organism         = 'hsa',
-                pAdjustMethod = "fdr",
-                pvalueCutoff  = 0.05,
-                qvalueCutoff  = 0.05)
+kegg_all_components <- do.call("rbind", kegg_dfs)
+write.table(format(kegg_all_components, digits=3), file=results_file_path, sep="\t", row.names = F)
 
-
-# Now display the results in various forms
-barplot(ekegg, showCategory=20)
-
-# Provide readable symbols
-ekegg_readable = setReadable(ekegg, 'org.Hs.eg.db', 'ENTREZID')
-
-# Make a gene concept network
-enrichplot::cnetplot(ekegg_readable)
-
-# Make a heatmap type plot
-heatplot(ekegg_readable)
-
-enrichplot::emapplot(ekegg_readable, piscale=1.5, layout='kk')
+# 
+# # Now display the results in various forms
+# barplot(ekegg, showCategory=20)
+# 
+# # Provide readable symbols
+# ekegg_readable = setReadable(ekegg, 'org.Hs.eg.db', 'ENTREZID')
+# 
+# # Make a gene concept network
+# enrichplot::cnetplot(ekegg_readable)
+# 
+# # Make a heatmap type plot
+# heatplot(ekegg_readable)
+# 
+# enrichplot::emapplot(ekegg_readable, piscale=1.5, layout='kk')
 
 print ("KEGG Gene Enrichment Analysis Done")
 
